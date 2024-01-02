2722d885b23819ce44bf77e711a131a6ed85dac5
ewkdev <j.schwgr@gmail.com>
2024-01-02 15:53:40 +0100

    Add optional dark mode to swagger documentation page;

--- templates/ca-project-template/CAWebProject.Api/Swagger/SwaggerUIMiddlewareExtentions.cs
+++ templates/ca-project-template/CAWebProject.Api/Swagger/SwaggerUIMiddlewareExtentions.cs
@@ -5,16 +5,26 @@ namespace CAWebProject.Api.Swagger;
 public static class SwaggerUIMiddlewareExtentions
 {
     public static IApplicationBuilder UseSwaggerDocumentation(
-        this IApplicationBuilder builder)
+        this IApplicationBuilder builder, bool useDarkmode = true)
     {
         var swaggerPath = Path.Combine(Directory.GetCurrentDirectory(), "Documentation", "Swagger");
+        var stylesPath = Path.Combine(Directory.GetCurrentDirectory(), "Documentation", "Swagger", "Styles");
         
         builder.UseStaticFiles(new StaticFileOptions
         {
             FileProvider = new PhysicalFileProvider(swaggerPath),
             RequestPath = new PathString("/swagger")
         });
-            
+
+        if (useDarkmode)
+        {
+            builder.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(stylesPath),
+                RequestPath = new PathString("/swagger/styles")
+            });
+        }
+
         builder.UseSwaggerUI(options =>
         {
             var files = Directory.GetFiles(swaggerPath, "*.swagger.json")
@@ -27,6 +37,12 @@ public static class SwaggerUIMiddlewareExtentions
                 var name = file.Name.Split(".").First();
                     
                 options.SwaggerEndpoint(url, name);
+            }
+
+            if (useDarkmode)
+            {
+                options.InjectStylesheet("/swagger/styles/Base.css");
+                options.InjectStylesheet("/swagger/styles/DarkTheme.css");
             }
         });
         
